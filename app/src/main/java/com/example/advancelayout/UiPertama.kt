de23b3ca7f3e7aebf68e6c809ef120263252528c
MalkingMan <arrayalkhozini@gmail.com>
2025-10-23 08:48:34 +0700

    Menambahkan import R

--- app/src/main/java/com/example/advancelayout/UiPertama.kt
+++ app/src/main/java/com/example/advancelayout/UiPertama.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import com.example.advancelayout.R
 
 
 @composable
