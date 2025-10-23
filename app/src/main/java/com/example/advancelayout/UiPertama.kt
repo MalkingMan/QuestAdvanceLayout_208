274e77e1c53c7fc4860e32ed36ce95fe3006a153
MalkingMan <arrayalkhozini@gmail.com>
2025-10-23 08:49:32 +0700

    Menambahkan import reference: Column, Alignment, dp, sp, dsb.

--- app/src/main/java/com/example/advancelayout/UiPertama.kt
+++ app/src/main/java/com/example/advancelayout/UiPertama.kt
@@ -9,6 +9,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import com.example.advancelayout.R
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 
 
 @composable
