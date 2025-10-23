ef41e4199a616312d54f4d3460dcb98a08711e54
MalkingMan <arrayalkhozini@gmail.com>
2025-10-23 19:28:50 +0700

    Membuat struktur dasar header dan footer halaman

--- app/src/main/java/com/example/advancelayout/UiPertama.kt
+++ app/src/main/java/com/example/advancelayout/UiPertama.kt
@@ -1,97 +1,38 @@
 package com.example.advancelayout
 
-import android.R.attr.fontFamily
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Card
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import com.example.advancelayout.R
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.material3.CardDefaults
-import androidx.compose.ui.graphics.Color
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.runtime.Composable
-
-
-
 
 @Composable
-fun ActivitasPertama(modifier: Modifier = Modifier ) {
-    Column(modifier = Modifier.padding(top = 100.dp)
-        .fillMaxSize(),
-    horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Text(text = stringResource(id = R.string.prodi),
-            fontSize = 35.sp,
-            fontWeight = FontWeight.Bold)
-        Text(text = stringResource(id = R.string.univ),
-            fontSize = 25.sp,)
-        Spacer(modifier = Modifier.height(height = 25.dp))
-        Card (modifier = Modifier
-            .fillMaxWidth(fraction = 1f)
-            .padding(all = 12.dp),
-            colors = CardDefaults.cardColors(containerColor = Color.DarkGray)
-
+fun ActivitasContoh() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 60.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Teknologi Informasi",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold
         )
-        {
-            Row(){
-                val gambar = painterResource(id = R.drawable.gambar)
-                Image(
-                    painter = gambar,
-                    contentDescription = null,
-                    modifier = Modifier.size(size = 100.dp).padding(all = 5.dp)
-                )
-                Spacer(modifier = Modifier.width(width = 30.dp))
-                Column(){
-                    Text(text = stringResource(id = R.string.nama),
-                        fontSize = 30.sp,
-                        fontFamily = FontFamily.Cursive,
-                        color = Color.White,
-                        modifier = Modifier.padding(top = 15.dp)
-                    )
+        Text(
+            text = "Universitas Muhammadiyah Yogyakarta",
+            fontSize = 18.sp
+        )
 
+        Spacer(modifier = Modifier.weight(1f))
 
-                }
-
-
-
-            }
-            Row() {
-                Text(
-                    text = stringResource(id = R.string.alamat),
-                    fontSize = 20.sp,
-                    color = Color.Yellow,
-                    modifier = Modifier.padding(top = 10.dp)
-                )
-            }
-
-        }
-
-
-    }
-    Box(
-        modifier = Modifier.fillMaxSize(),
-    ){
-     Text(
-         text = stringResource(id = R.string.copy),
-         modifier = Modifier
-             .align(alignment = Alignment.BottomCenter)
-             .padding(all = 50.dp)
-     )
+        Text(
+            text = "copyright, 2025",
+            fontSize = 12.sp,
+            modifier = Modifier.padding(bottom = 20.dp)
+        )
     }
 }
