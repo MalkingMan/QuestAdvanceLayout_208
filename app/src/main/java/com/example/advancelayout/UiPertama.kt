f547ba89e8fea4c81ab513f1106b40bc57e5d778
MalkingMan <arrayalkhozini@gmail.com>
2025-10-23 19:49:11 +0700

    Mengisi alamat pada card ketiga

--- app/src/main/java/com/example/advancelayout/UiPertama.kt
+++ app/src/main/java/com/example/advancelayout/UiPertama.kt
@@ -131,7 +131,7 @@ fun ActivitasContoh(modifier: Modifier = Modifier) {
                 Spacer(modifier = Modifier.width(20.dp))
                 Column {
                     Text("Carlos Juanito", fontSize = 20.sp, color = Color.White, fontWeight = FontWeight.Bold)
-                    Text("Isi Alamat", fontSize = 16.sp, color = Color.Cyan)
+                    Text("Maluku", fontSize = 16.sp, color = Color.Cyan)
                     Text("Isi No Handphone", fontSize = 14.sp, color = Color.White)
                 }
             }
